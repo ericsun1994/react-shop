c5f26ffc64666cdcb03ab15110bd21d926e3b8e7
Jongsoo_sun <ericsun159@gmail.com>
2022-10-28 14:31:36 +0900

    styled-component setting & practice

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,4 +1,15 @@
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+const YellowBtn = styled.button`
+  background: ${(props) => props.bg};
+  color: ${(props) => (props.bg == "blue" ? "white" : "black")};
+  padding: 10px;
+`;
+
+let NewBtn = styled.button(YellowBtn)`
+  color
+`;
 
 function Detail(props) {
   let { id } = useParams();
@@ -10,6 +21,8 @@ function Detail(props) {
 
   return (
     <div className="container">
+      <YellowBtn bg="blue">버튼</YellowBtn>
+      <YellowBtn bg="orange">버튼</YellowBtn>
       <div className="row">
         <div className="col-md-6">
           <img
