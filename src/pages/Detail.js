a69d2eae1a317c299c7ba5b35571d427338f55c3
Jongsoo_sun <ericsun159@gmail.com>
2022-11-02 19:25:45 +0900

    add detail page

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,5 +1,8 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
+
+import { Nav } from "react-bootstrap";
 
 const YellowBtn = styled.button`
   background: ${(props) => props.bg};
@@ -7,21 +10,30 @@ const YellowBtn = styled.button`
   padding: 10px;
 `;
 
-let NewBtn = styled.button(YellowBtn)`
-  color
-`;
-
 function Detail(props) {
   let { id } = useParams();
-  console.log(id);
+  let findItem = props.shoes.foind((x) => x.id === id);
+  //   let findItem = props.shoes.find(function (x) {
+  //     return x.id == id;
+  //   });
 
-  let findItem = props.shoes.find(function (x) {
-    return x.id == id;
-  });
+  let [count, setCount] = useState(0);
+  let [alert, setAlert] = useState(0);
+
+  useEffect(() => {
+    let a = setTimeout(() => {
+      setAlert(false);
+    }, 2000); // 스위치 조작방식
+    return () => {
+      clearTimeout(a); // 타이머 제거해주는 함수임
+    };
+  }, []); // useEffect 실행 조건 넣을 수 있는 곳은 [] (count 변수가 변할때 마다 실행)
 
   return (
     <div className="container">
-      <YellowBtn bg="blue">버튼</YellowBtn>
+      {alert === true ? (
+        <div className="alert alert-warning">2초이내 구매시 할인</div>
+      ) : null}
       <YellowBtn bg="orange">버튼</YellowBtn>
       <div className="row">
         <div className="col-md-6">
@@ -37,6 +49,20 @@ function Detail(props) {
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
+
+      <Nav variant="tabs" defaultActiveKey="/home">
+        <Nav.Item>
+          <Nav.Link href="/home">Active</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">Option 2</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="disabled" disabled>
+            Disabled
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
     </div>
   );
 }
